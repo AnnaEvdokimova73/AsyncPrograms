4ed832d2d74d8d5da756ad8af1789488822cd5c7
Anna Evdokimova <evdockimova.annie@yandex.ru>
2024-05-16 23:23:09 +0300

    Initial commit

---
+++ AsyncForEach/AsyncForEach.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <future>
+
+int blockCount = 4;
+
+void printBlockPart(std::vector<int>::iterator beg, std::vector<int>::iterator end)
+{
+    for (auto it = beg; it < end; ++it)
+    {
+        std::cout << *it << " ";
+    }
+}
+
+void forEach(std::vector<int> vec, int beg, int end)
+{
+    std::vector<int> vec1;
+    std::vector<int> vec2;
+    int endNew = (end % 2 != 0) ? end / 2 + 1 : end / 2;
+
+    if (vec.size() == 1 || vec.size() == 2 || vec.size() == 3)
+    {
+        std::async(std::launch::async, printBlockPart, vec.begin(), vec.end());
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        return;
+    }
+    for (auto i = 0; i < endNew; ++i)
+    {
+        vec1.push_back(vec.at(i));
+    }
+    for (auto i = endNew; i < end; ++i)
+    {
+        vec2.push_back(vec.at(i));
+    }
+    forEach(vec1, beg, endNew);
+    forEach(vec2, endNew, vec2.size());
+
+    //std::async(std::launch::async, printBlockPart, vec1.begin(), vec1.end());
+    //std::async(std::launch::async, printBlockPart, vec2.begin(), vec2.end());
+
+    return;
+}
+
+int main()
+{
+    std::vector<int> vec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    forEach(vec, 0, vec.size());
+}
---
+++ ChoiceSort/ChoiceSort.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <array>
+#include <thread>
+#include <future>
+#include <utility>
+
+const int ARR_SIZE = 6;
+
+template<int SIZE>
+void getIndMin(std::promise<int> pVal, std::array<int, SIZE>& arr, int start, int end)
+{
+    int min = arr[start];
+    int indMin = start;
+    for (int i = start + 1; i <= end; ++i)
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+            indMin = i;
+        }
+    }
+    pVal.set_value(indMin);
+}
+
+template<int SIZE>
+void choiceSort(std::array<int, SIZE>& arr, int start)
+{
+    int end = arr.size() - 1;
+    if (start == end)
+    {
+        return;
+    }
+    std::promise<int> promVal;
+    std::future<int> futVal{ promVal.get_future() };
+    std::thread threadSearch(getIndMin<ARR_SIZE>, std::move(promVal), std::ref(arr), start, end);
+    threadSearch.join();
+    int indMin{ futVal.get() };
+    std::swap(arr[start], arr[indMin]);
+    choiceSort(arr, start + 1);
+    return;
+}
+
+int main()
+{
+    std::array<int, ARR_SIZE> arr{ 4, 3, 7, 2, 1, 5 };
+    choiceSort(arr, 0);
+    std::cout << "Hi";
+}
